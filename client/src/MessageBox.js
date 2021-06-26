f04cf58dc59f3b0904538a41d4067f64f1c4f648
Julius Lee <7julius.lee@gmail.com>
2021-06-26 15:09:05 -0700

    showing socketid

--- client/src/MessageBox.js
+++ client/src/MessageBox.js
@@ -2,12 +2,17 @@ import { useContext, useEffect, useState } from 'react';
 import { context } from './App';
 
 const MessageBox = () => {
+    const [username, setUsername] = useState('');
+
     const socket = useContext(context);
     const [message, setMessage] = useState('');
+    const [chatHistory, setChatHistory] = useState([]);
 
     useEffect(() => {
-        socket.emit('chat message', 'hello');
-        socket.on('broadcast message', (msg) => console.log(msg));
+        socket.on('broadcast message', (msg) =>
+            setChatHistory((prev) => [...prev, msg])
+        );
+        socket.on('connected', ({ id }) => setUsername(id));
     }, [socket]);
 
     const sendMessage = (msg) => {
@@ -17,6 +22,10 @@ const MessageBox = () => {
 
     return (
         <div>
+            {`Username: ${username}`}
+            {chatHistory.map((chat, i) => (
+                <p key={i + chat}>{chat}</p>
+            ))}
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
