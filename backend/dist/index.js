f4992df6c726971bfbd8628f9cc9de2b846e7dba
Julius Lee <7julius.lee@gmail.com>
2021-06-27 01:42:52 -0700

    resetup tsc

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,79 +8,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var cors_1 = __importDefault(require("cors"));
-var app = express_1.default();
+exports.io = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = express_1.default();
 app.use(cors_1.default);
-var http_1 = __importDefault(require("http"));
-var server = http_1.default.createServer(app);
-var socket_io_1 = require("socket.io");
-var io = new socket_io_1.Server(server, {
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer(app);
+const socket_io_1 = require("socket.io");
+const socketActions_1 = require("./utils/socketActions");
+exports.io = new socket_io_1.Server(server, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST']
     }
 });
-var users = [];
-var rooms = [];
-io.on('connection', function (socket) { return __awaiter(void 0, void 0, void 0, function () {
-    var _a, _b;
-    return __generator(this, function (_c) {
-        switch (_c.label) {
-            case 0:
-                socket.join('room1');
-                console.log('a user connected: ', socket.id, socket.rooms);
-                _b = (_a = console).log;
-                return [4 /*yield*/, io.sockets.allSockets()];
-            case 1:
-                _b.apply(_a, [_c.sent()]);
-                socket.on('login', function (msg) {
-                    if (!msg) {
-                        return socket.emit('connected', { id: "GUEST(" + socket.id + ")" });
-                    }
-                    socket.emit('connected', { id: msg });
-                });
-                socket.on('chat message', function (msg) {
-                    io.to('room1').emit('broadcast message', msg.user.name + ": " + msg.message);
-                });
-                socket.on('disconnect', function () {
-                    console.log('user disconnected');
-                });
-                return [2 /*return*/];
+// interface User {
+//     id: string;
+//     name: string;
+// }
+exports.io.on('connection', (socket) => __awaiter(void 0, void 0, void 0, function* () {
+    socket.join('global');
+    console.log('a user connected: ', socket.id, socket.rooms);
+    socket.on('create-new-room', (roomName) => __awaiter(void 0, void 0, void 0, function* () {
+        if (!roomName || roomName.trim() === '') {
+            return;
         }
+        yield socket.join(roomName);
+        socket.to(roomName).emit('new-user-joined', socket.id);
+    }));
+    socket.on('login', (msg) => {
+        console.log(msg);
     });
-}); });
-server.listen(3003, function () {
+    socket.on('chat message', socketActions_1.broadcast);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+}));
+server.listen(3003, () => {
     console.log('listening on port 3003');
 });
+//# sourceMappingURL=index.js.map
