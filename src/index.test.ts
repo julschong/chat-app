8f25c80425206509afa26c5825291a090faddd03
Julius Lee <7julius.lee@gmail.com>
2021-06-29 00:52:33 -0700

    setup first test for login

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,31 +1,30 @@
 import { Socket } from 'socket.io';
 
-import { createServer } from 'http';
+import { createServer, Server as hpServer } from 'http';
 import { Server } from 'socket.io';
 import { Socket as ClientSocket, io as ClientIO } from 'socket.io-client';
 import { AddressInfo } from 'net';
+import { LoginMessage, userLogin } from './socketEvents/socketActions';
+import { users, User } from './_data/user';
+let io: Server,
+    serverSocket: Socket,
+    clientSocket: ClientSocket,
+    httpServer: hpServer;
+
+beforeAll((done) => {
+    httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+        const port = (httpServer.address() as AddressInfo).port;
+        clientSocket = ClientIO(`http://localhost:${port}`);
+        io.on('connection', (socket) => {
+            serverSocket = socket;
+        });
+        clientSocket.on('connect', done);
+    });
+});
 
 describe('my awesome project', () => {
-    let io: Server, serverSocket: Socket, clientSocket: ClientSocket;
-
-    beforeAll((done) => {
-        const httpServer = createServer();
-        io = new Server(httpServer);
-        httpServer.listen(() => {
-            const port = (httpServer.address() as AddressInfo).port;
-            clientSocket = ClientIO(`http://localhost:${port}`);
-            io.on('connection', (socket) => {
-                serverSocket = socket;
-            });
-            clientSocket.on('connect', done);
-        });
-    });
-
-    afterAll(() => {
-        io.close();
-        clientSocket.close();
-    });
-
     test('should work', (done) => {
         clientSocket.on('hello', (arg) => {
             expect(arg).toBe('world');
@@ -43,4 +42,29 @@ describe('my awesome project', () => {
             done();
         });
     });
+});
+
+describe('my test', () => {
+    test('testing', (done) => {
+        const loginData: LoginMessage = {
+            id: '1',
+            name: 'Julius'
+        };
+
+        serverSocket.on('login', async function (loginData: LoginMessage) {
+            await userLogin(loginData, serverSocket);
+            expect(users).toHaveLength(1);
+            expect(Object.keys(users[0])).toContain('clientId');
+            expect(Object.keys(users[0])).toContain('name');
+            expect(Object.keys(users[0])).toContain('currentRoom');
+            expect(Object.keys(users[0])).toContain('serverId');
+            done();
+        });
+        clientSocket.emit('login', loginData);
+    });
+});
+
+afterAll(() => {
+    io.close();
+    clientSocket.close();
 });
