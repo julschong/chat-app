8e1af41d6bd847c898694324ae1a5fd411780cb9
Julius Lee <7julius.lee@gmail.com>
2021-08-29 21:36:28 -0700

    added client url to cors on backend

--- dist/index.js
+++ dist/index.js
@@ -14,14 +14,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.io = void 0;
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default());
 const http_1 = __importDefault(require("http"));
 const server = http_1.default.createServer(app);
 const socket_io_1 = require("socket.io");
 const socketActions_1 = require("./socketEvents/socketActions");
 exports.io = new socket_io_1.Server(server, {
     cors: {
-        origin: '*',
+        origin: 'https://jc-chat-app.netlify.app',
         methods: ['GET', 'POST']
     }
 });
